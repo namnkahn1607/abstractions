9b003bc3b7c17c27b78df70bd371e2897bc45a22
namnkahn1607 <nlnkandthebois@gmail.com>
2025-10-26 18:43:20 +0700

    26 Oct 25 (not again) I swear this is the last time I play around gotta get to study for exams soon

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 //TIP With Search Everywhere, you can find any action, file, or symbol in your project. Press <shortcut actionId="Shift"/> <shortcut actionId="Shift"/>, type in <b>terminal</b>, and press <shortcut actionId="EditorEnter"/>. Then run <shortcut raw="npm run dev"/> in the terminal and click the link in its output to open the app in the browser.
-export function setupCounter(element: HTMLElement) {
+const setupCounter = (element: HTMLElement) => {
   //TIP Try <shortcut actionId="GotoDeclaration"/> on <shortcut raw="counter"/> to see its usages. You can also use this shortcut to jump to a declaration – try it on <shortcut raw="counter"/> on line 13.
   let counter = 0;
 
@@ -12,16 +12,26 @@ export function setupCounter(element: HTMLElement) {
   const setCounter = (value: number) => {
     counter = adjustCounterValue(value);
     //TIP WebStorm has lots of inspections to help you catch issues in your project. It also has quick fixes to help you resolve them. Press <shortcut actionId="ShowIntentionActions"/> on <shortcut raw="text"/> and choose <b>Inline variable</b> to clean up the redundant code.
-    const text = `${counter}`;
-    element.innerHTML = text;
+
+    element.innerHTML = `${counter}`;
   };
 
-  document.getElementById('increaseByOne')?.addEventListener('click', () => setCounter(counter + 1));
-  document.getElementById('decreaseByOne')?.addEventListener('click', () => setCounter(counter - 1));
-  document.getElementById('increaseByTwo')?.addEventListener('click', () => setCounter(counter + 2));
+  document.getElementById('increaseByOne')?.addEventListener(
+      'click', () => setCounter(counter + 1)
+  );
+
+  document.getElementById('decreaseByOne')?.addEventListener(
+      'click', () => setCounter(counter - 1)
+  );
+
+  document.getElementById('increaseByTwo')?.addEventListener(
+      'click', () => setCounter(counter + 2)
+  );
 
   //TIP In the app running in the browser, you’ll find that clicking <b>-2</b> doesn't work. To fix that, rewrite it using the code from lines 19 - 21 as examples of the logic.
-  document.getElementById('decreaseByTwo')
+  document.getElementById('decreaseByTwo')?.addEventListener(
+      'click', () => setCounter(counter - 2)
+  );
 
   //TIP Let’s see how to review and commit your changes. Press <shortcut actionId="GotoAction"/> and look for <b>commit</b>. Try checking the diff for a file – double-click main.ts to do that.
   setCounter(0);
@@ -31,3 +41,4 @@ export function setupCounter(element: HTMLElement) {
 setupCounter(document.getElementById('counter-value') as HTMLElement);
 
 //TIP There's much more in WebStorm to help you be more productive. Press <shortcut actionId="Shift"/> <shortcut actionId="Shift"/> and search for <b>Learn WebStorm</b> to open our learning hub with more things for you to try.
+export { setupCounter };
